07dfa5dea49dc95226bdf0fb6c9a92f46f99c2de
DogukanGun <dogukangundogan5@gmail.com>
2025-03-08 15:51:19 +0100

    Add loader for navigation

--- frontend_app/app/template.tsx
+++ frontend_app/app/template.tsx
@@ -1,22 +1,12 @@
 "use client";
-import { useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
-import LoadingOverlay from "./components/LoadingOverlay";
+import NavigationLoader from "./components/NavigationLoader";
+import { NavigationLoadingProvider } from "./context/NavigationLoadingContext";
 
 export default function Template({ children }: { children: React.ReactNode }) {
-  const [isLoading, setIsLoading] = useState(false);
-  const pathname = usePathname();
-
-  useEffect(() => {
-    setIsLoading(true);
-    const timer = setTimeout(() => setIsLoading(false), 300);
-    return () => clearTimeout(timer);
-  }, [pathname]);
-
   return (
-    <>
-      {isLoading && <LoadingOverlay />}
+    <NavigationLoadingProvider>
+      <NavigationLoader />
       {children}
-    </>
+    </NavigationLoadingProvider>
   );
 }
